4cf752351b25fcab17bb1b73748755c5abb86a91
Jamie Pinkham <jamie.pinkham@bottlerocketapps.com>
2013-07-18 11:33:48 -0400

    doing it right

--- TableViewAnimationTest/ViewController.m
+++ TableViewAnimationTest/ViewController.m
@@ -7,9 +7,6 @@
 //
 
 
-#warning resizing works when the animations are added in cellforrow, but wonky shit happens when you add the animations in  viewdidappear
-
-
 #import "ViewController.h"
 #import <QuartzCore/QuartzCore.h>
 @interface ViewController ()
@@ -31,25 +28,6 @@ float const CELL_ANIMATION_DELAY = 0.05f;
     // Dispose of any resources that can be recreated.
 }
 
--(void)viewWillAppear:(BOOL)animated
-{
-    
-}
-
--(void)viewDidAppear:(BOOL)animated
-{
-#warning remove comment and add comment to if statement in cellforrow
-    //animate cells that are visible
-    int count = 0;
-    for(UITableViewCell *cell in self.menuTableView.visibleCells)
-    {
-        UIView *subView = [cell viewWithTag:CELL_VIEW_TAG];
-        CALayer *sublayer = subView.layer;
-        [self addAnimationsToLayer:sublayer count:count];
-        count++;
-    }
-     
-}
 
 #pragma mark - Table view data source
 
@@ -83,7 +61,7 @@ float const CELL_ANIMATION_DELAY = 0.05f;
     if(tmpV != nil)
        [tmpV removeFromSuperview];
 
-    UIView *cellUIView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 37, 37)];
+    UIView *cellUIView = [[UIView alloc] initWithFrame:CGRectMake(CGRectGetMaxX(cell.contentView.frame) - 37-10, 0, 37, 37)];
     cellUIView.tag = 1;
     
     
@@ -92,86 +70,23 @@ float const CELL_ANIMATION_DELAY = 0.05f;
     CALayer *sublayer = [CALayer layer];
     sublayer.contents = (id)img.CGImage;
     sublayer.frame = CGRectMake(0, 0, 37, 37);
-    sublayer.position = CGPointMake(cell.frame.size.width - (sublayer.frame.size.width/2) - 10, (sublayer.frame.size.width/2) + 10);
+
+	cellUIView.transform =	CGAffineTransformTranslate(CGAffineTransformMakeScale(0.3, 0.3), -1000, 0);
+	[UIView animateWithDuration:1.8f delay:CELL_ANIMATION_DELAY * indexPath.row options:0 animations:^{
+		cellUIView.transform = CGAffineTransformIdentity;
+	} completion:^(BOOL finished) {
+		;
+	}];
     
-    
-    
-    if(shouldDoTableAnimations)
-    {
-#warning remove comment and comment out viewdidappear
-        //[self addAnimationsToLayer:sublayer count:indexPath.row];
-    }
-    
+
     [cellUIView.layer addSublayer:sublayer];
-    //[cell.layer addSublayer:sublayer];
     
     [cell addSubview:cellUIView];
-    
-    //cell.textLabel.text = [NSString stringWithFormat:@"%d", indexPath.row +1];
-    
-    // Configure the cell...
-    //NSLog(@"%f", cell.frame.size.width);
+
 
     return cell;
 }
 
--(void) addAnimationsToLayer:(CALayer *)sublayer count:(int)count
-{
-    sublayer.opacity = 1.0f;
-    float delay = CELL_ANIMATION_DELAY * (count);
-    
-    //[sublayer addAnimation:[self opacityAnimation:0.0f toValue:1.0f delay:delay] forKey:@"opacityAnimation"];
-    //[sublayer addAnimation:[self positionAnimation:CGPointMake(0, 0) endPoint:sublayer.position delay:delay] forKey:@"positionAnimation"];
-    [sublayer addAnimation:[self frameAnimation:CGRectMake(0, 0, 0, 0) endFrame:sublayer.frame delay:delay] forKey:@"frameAnimation"];
-}
-
--(CABasicAnimation *)opacityAnimation:(float)fromValue toValue:(float)toValue delay:(float)delay
-{
-    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
-    anim.fromValue = [NSNumber numberWithFloat:fromValue];
-    anim.toValue = [NSNumber numberWithFloat:toValue];
-    anim.removedOnCompletion = NO;
-    [anim setFillMode:kCAFillModeForwards];
-    anim.duration = 2.6f;
-    anim.delegate = self;
-    anim.beginTime = CACurrentMediaTime() + delay;
-    anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionDefault];
-    return anim;
-}
-
-- (CABasicAnimation *)positionAnimation:(CGPoint)startPoint
-                               endPoint:(CGPoint)endPoint
-                                  delay:(float)delay
-{
-    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"position"];
-    anim.fromValue = [NSValue valueWithCGPoint:startPoint];
-    anim.toValue = [NSValue valueWithCGPoint:endPoint];
-    anim.removedOnCompletion = NO;
-    [anim setFillMode:kCAFillModeForwards];
-    [anim setDuration:0.8f];
-    [anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionDefault]];
-    anim.repeatCount = 1;
-    anim.beginTime = CACurrentMediaTime() + delay;
-    anim.delegate = self;
-    return anim;
-}
-
-- (CABasicAnimation *)frameAnimation:(CGRect)startFrame
-                               endFrame:(CGRect)endFrame
-                                  delay:(float)delay
-{
-    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"bounds"];
-    anim.fromValue = [NSValue valueWithCGRect:startFrame];
-    anim.toValue = [NSValue valueWithCGRect:endFrame];
-    anim.removedOnCompletion = NO;
-    [anim setFillMode:kCAFillModeForwards];
-    [anim setDuration:1.8f];
-    [anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionDefault]];
-    anim.repeatCount = 1;
-    anim.beginTime = CACurrentMediaTime() + delay;
-    anim.delegate = self;
-    return anim;
-}
 
 
 /*
